374d143b0584af97fceb22d5c002573e5ee46134
Karina <karina.steinberg@voco.ee>
2024-05-27 08:31:48 +0300

    Added manualy triggered errors

--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpNotFoundException;
 
 class AlbumsController extends Controller
 {
@@ -19,6 +20,9 @@ class AlbumsController extends Controller
 
         $key = array_search($args['id'], array_column(
             $albums, 'id'));
+        if ($key === false) {
+            throw new HttpNotFoundException($request, $response);
+        }
 
         return $this->render($response, 'details.html', ['album' => $albums[$key]]);
     }
