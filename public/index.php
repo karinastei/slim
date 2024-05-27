fe9a9cc4ec050574a5504f00e045d7c4d0fa0e27
Karina <karina.steinberg@voco.ee>
2024-05-27 08:17:10 +0300

    Added error handling

--- public/index.php
+++ public/index.php
@@ -23,8 +23,17 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', 'App\Controller\AlbumsController:default');
-$app->get('/details/{id}', 'App\Controller\AlbumsController:details');
+app->get('/details/{id:[0-9]+}', 'App\Controller\AlbumsController:details');
 $app->get('/search', 'App\Controller\AlbumsController:search');
 $app->any('/form', 'App\Controller\AlbumsController:form');
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setErrorHandler(
+    Slim\Exception\HttpNotFoundException::class,
+    function (Psr\Http\Message\ServerRequestInterface $request) use ($container) {
+        $controller = new App\Controller\ExceptionController($container);
+        return $controller->notFound($request);
+    }
+);
 
 $app->run();
